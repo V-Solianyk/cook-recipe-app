6d53857e336b5000de3b787d3f739ee089459b47
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-09-08 15:47:24 +0300

    first commit

---
+++ src/main/java/com/example/cookrecipeappilication/dto/RecipeResponseDto.java
@@ -0,0 +1,2 @@
+package com.example.cookrecipeappilication.dto;public class RecipeResponseDto {
+}
---
+++ src/main/java/com/example/cookrecipeappilication/mapper/impl/RecipeRequestDtoMapper.java
@@ -0,0 +1,2 @@
+package com.example.cookrecipeappilication.mapper.impl;public class RecipeRequestDtoMapper {
+}
